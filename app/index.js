755ddd11a0eb1fa5f86cf7cacd5187518a6a31bb
Prakash Paudel <total.prakash@gmail.com>
2016-03-26 15:22:58 -0500

    added react-hot-loader

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 ReactDOM.render(
-	<div>Hello World</div>,
+	<div>Hello world</div>,
 	document.getElementById('app')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
@@ -8,6 +9,8 @@ var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
 
 module.exports = {
   entry: [
+		'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+		'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
     './app/index.js'
   ],
   output: {
@@ -16,8 +19,11 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+      {test: /\.js$/, exclude: /node_modules/, loaders: ["react-hot", "babel-loader"]}
     ]
   },
-  plugins: [HTMLWebpackPluginConfig]
+  plugins: [
+  	HTMLWebpackPluginConfig,
+  	new webpack.HotModuleReplacementPlugin()
+  ]
 };
